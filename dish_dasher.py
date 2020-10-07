8099f3082edd9189ad5ed253f830052ccd1be36e
David Anyaeche <davidio7943@gmail.com>
2020-10-07 22:04:41 +0000

    Initial commit

---
+++ dish_dasher.py
@@ -0,0 +1,39 @@
+
+class dish_dasher(object):
+  def __init__(self):
+    self.dish_repository = {}
+
+  
+  def add_dish(self, dish):
+    if type(dish) != str:
+        print("Dish must be valid cusine 😡")
+    elif  dish.isalpha() == False:
+        print("Dish must be valid cusine 😡")     
+    else:
+        dish = dish.upper()
+        dish = dish.replace(" ","")
+        if dish not in self.dish_repository:
+            print("New Dish added to Dish Dasher! 😁")
+            self.dish_repository[dish] = True
+        else:
+            print("Dish is already added to Dish Dasher 😅")
+
+  def delete_dish(self, dish):
+    if type(dish) != str:
+        print("Dish must be valid cusine 😡")
+    elif dish.isalpha() == False:
+        print("Dish must be valid cusine 😡")
+    else:
+        dish = dish.upper()
+        if dish not in self.dish_repository or self.dish_repository[dish] == False:
+            print("Dish currently isn't in Dish Dasher! 😅")
+        else:
+            print("Dish is deleted from Dish Dasher 😁")
+            self.dish_repository[dish] = False
+
+  
+  def view_dishes(self):
+    print("Viewing Dish Dash catalog 👍")
+    for key, values in self.dish_repository.items():
+      if values == True:
+        print(key)
---
+++ main.py
@@ -0,0 +1,23 @@
+import dish_dasher
+
+def main():
+  dish_dasher_instance = dish_dasher()
+  try:
+    input_file = open('input.txt', 'r')
+    for line in input_file:
+      line_list = line.split(" ")
+      if len(line_list) == 2 and line_list[0] == 'ADD':
+        dish_dasher_instance.add_dish(line_list[1])
+      elif len(line_list) == 2 and line_list[0] == 'DELETE':
+        dish_dasher_instance.delete_dish(line_list[1])
+      elif len(line_list) == 1 and line_list[0] == 'VIEW':
+        dish_dasher_instance.view_dish(line_list[1])
+      else:
+        continue
+  except Exception as e:
+    print (e)
+  finally:
+    input_file.close()
+
+if __name__ == "__main__": 
+  main()
